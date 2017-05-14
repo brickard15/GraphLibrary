d3861c62e8da36cc413569ba5f49a2e5b61a857e
brickard15 <brian.rick@gmail.com>
2017-05-14 12:34:19 -0500

    Made method parameters final in the Graph Interface.
    I made all the parameters in the method definitions final. I did this to
    keep with good practices of strong static typing.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,7 +1,7 @@
 public interface Graph <NodeType, EdgeType>{
 
-    public boolean hasNode(NodeType node);
-    public boolean hasEdge(NodeType node1, NodeType node2);
-    public void addNode(NodeType node);
-    public void addEdge(NodeType node1, NodeType node2);
+    public boolean hasNode(final NodeType node);
+    public boolean hasEdge(final NodeType node1, final NodeType node2);
+    public void addNode(final NodeType node);
+    public void addEdge(final NodeType node1, final NodeType node2);
 }
