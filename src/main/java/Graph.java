79967482c45a4a4239fbf8c12a74c13a61b68640
brickard15 <brian.rick@gmail.com>
2017-05-14 12:08:40 -0500

    Added a definition for addNode method to the Graph Interface.
    I added a addNode void method definition that takes a node as a parameter.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -5,4 +5,5 @@ public interface Graph <NodeType, EdgeType>{
     public boolean hasNode(NodeType node);
     public boolean hasEdge(NodeType node1, NodeType node2);
     public Optional<EdgeType> getEdge(NodeType node1, NodeType node2);
+    public void addNode(NodeType node);
 }
