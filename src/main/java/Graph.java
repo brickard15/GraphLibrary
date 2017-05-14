e3cc4e846b72c5f2488c9a09da5191f122ddcafa
brickard15 <brian.rick@gmail.com>
2017-05-14 12:05:34 -0500

    Added method getEdge definition to the Graph Interface.
    I added a definition for a getEdge method. The method takes two nodes,
    and returns an Optional of EdgeType. I'm not sure that you should be able
    to get an Edge out of a graph, but I'll leave it for now.

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,5 +1,8 @@
+import java.util.Optional;
+
 public interface Graph <NodeType, EdgeType>{
 
     public boolean hasNode(NodeType node);
     public boolean hasEdge(NodeType node1, NodeType node2);
+    public Optional<EdgeType> getEdge(NodeType node1, NodeType node2);
 }
