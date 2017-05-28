0e0f403e4460e818349a9e14dac613b5590d1bf7
brickard15 <brian.rick@gmail.com>
2017-05-28 10:26:37 -0500

    Removed the addEdge method, and changed method definition.
    Now there is only one addEdge method that takes a node ID.

--- src/main/java/Nodes/Node.java
+++ src/main/java/Nodes/Node.java
@@ -2,11 +2,7 @@ package Nodes;
 
 public class Node <NodeType>{
 
-    public void addEdge(int nodeID1, int nodeID2){
-
-    }
-
-    public void addEdge(NodeType node){
+    public void addEdge(int nodeID){
 
     }
 
