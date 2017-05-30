eac400045b6edfb105fc22e85cb5bf4869e1e30f
brickard15 <brian.rick@gmail.com>
2017-05-30 16:24:20 -0500

    Implemented the addEdge and hasEdge methods.
    I implemented the addEdge method. In doing so I changed the edges variable
    to a List from a HashMap. In order to to allow multiple edges to and from
    the same nodes the data structure has to be a list and not a HashMap.
    
    I implement the hasEdge method. The method checks the to see if the parameter
    matches any of the ID of other edges.

--- src/main/java/Nodes/Node.java
+++ src/main/java/Nodes/Node.java
@@ -1,25 +1,45 @@
 package Nodes;
 
-import java.util.HashMap;
+import fj.P2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node <NodeDataType, NodeIDType, EdgeType>{
-
     private final NodeDataType nodeData;
-    private final HashMap<Integer, EdgeType> edges;
+    private final List<P2<NodeIDType, EdgeType>> edges;
     private final NodeIDType nodeID;
 
     public Node(NodeDataType nodeData, NodeIDType nodeID){
         this.nodeData = nodeData;
-        edges = new HashMap<>();
+        edges = new ArrayList<>();
         this.nodeID = nodeID;
     }
 
-    public void addEdge(int otherNodeID, EdgeType edgeData){
+    public void addEdge(NodeIDType otherNodeID, EdgeType edgeData){
+        edges.add(new P2<NodeIDType, EdgeType>() {
+            @Override
+            public NodeIDType _1() {
+                return otherNodeID;
+            }
 
+            @Override
+            public EdgeType _2() {
+                return edgeData;
+            }
+        });
     }
 
-    public boolean hasEdge(int otherNodeID){
-        return false;
+    public boolean hasEdge(NodeIDType otherNodeID){
+        final boolean[] result = {false};
+
+        edges.forEach(edgeTuple -> {
+            if (edgeTuple._1().equals(otherNodeID)){
+                result[0] = true;
+            }
+        });
+
+        return result[0];
     }
 
     //TODO: Maybe this should just be an index.
