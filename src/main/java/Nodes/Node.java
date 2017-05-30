4bbcfdc3c21ae1140399a4b85e27cf4143f9901d
brickard15 <brian.rick@gmail.com>
2017-05-30 15:20:16 -0500

    Added a HashMap that will represent edges, and some TODOs about the getID method.
    I created a HashMap called edges that will represent ID of a Node, and the edge value.
    
    I also added a todo about how the ID could possible work.

--- src/main/java/Nodes/Node.java
+++ src/main/java/Nodes/Node.java
@@ -1,11 +1,15 @@
 package Nodes;
 
+import java.util.HashMap;
+
 public class Node <NodeType, EdgeType>{
 
     private final NodeType nodeData;
+    private final HashMap<Integer, EdgeType> edges;
 
-    private Node(NodeType nodeData){
+    public Node(NodeType nodeData){
         this.nodeData = nodeData;
+        edges = new HashMap<>();
     }
 
     public void addEdge(int otherNodeID, EdgeType edgeData){
@@ -16,6 +20,8 @@ public class Node <NodeType, EdgeType>{
         return false;
     }
 
+    //TODO: Maybe this should just be an index.
+    //TODO: Also maybe it should return an optional? But optionals don't work with ints.
     public int getNodeID(){
         return 0;
     }
