9e5f95e33eba848b4ac06cc998a3a11a86e46636
brickard15 <brian.rick@gmail.com>
2017-05-30 17:17:17 -0500

    Created a method to test the addEdge method in the Node class.
    I created an addEgeWithOneEdgeTest method to test the addEdge method of the
    Node class.

--- src/test/java/Node/NodeTest.java
+++ src/test/java/Node/NodeTest.java
@@ -1,4 +1,21 @@
 package Node;
 
+import fj.P2;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
 public class NodeTest {
+
+    @Test
+    public void addEdgeWithOneEdgeTest(){
+        Node<Integer, Integer, Integer> node = new Node(1, 0);
+        node.addEdge(2, 1);
+
+        List<P2<Integer, Integer>> possibleEdges = node.getEdges();
+
+        assertEquals(2, possibleEdges.get(0)._1(), 0.0);
+    }
 }
