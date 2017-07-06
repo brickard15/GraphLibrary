084bde04618c1a12405d58f9ed283de0219b80b9
brickard15 <brian.rick@gmail.com>
2017-07-05 23:47:29 -0500

    Changed the hasNode definition.
    The new hasNode definition takes a NodeId.

--- src/main/java/Graphs/Graph.java
+++ src/main/java/Graphs/Graph.java
@@ -2,7 +2,7 @@ package Graphs;
 
 public interface Graph <NodeType, NodeIdType, EdgeType>{
 
-    public boolean hasNode(final NodeType node);
+    public boolean hasNode(final NodeIdType nodeId);
     public boolean hasEdge(final NodeType node1, final NodeType node2);
     public void addNode(final NodeType node);
     public void addEdge(final NodeType node1, final NodeType node2);
