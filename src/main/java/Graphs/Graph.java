de5771529865e7f85dcf7e9bf93d0a1b015d77dd
brickard15 <brian.rick@gmail.com>
2017-07-05 23:52:13 -0500

    Changed the addNode definition to take nodeData and a nodeId.

--- src/main/java/Graphs/Graph.java
+++ src/main/java/Graphs/Graph.java
@@ -4,6 +4,6 @@ public interface Graph <NodeType, NodeIdType, EdgeType>{
 
     public boolean hasNode(final NodeIdType nodeId);
     public boolean hasEdge(final NodeIdType nodeIdType1, final NodeIdType nodeIdType2);
-    public void addNode(final NodeType node);
+    public void addNode(final NodeType nodeData, NodeIdType nodeId);
     public void addEdge(final NodeType node1, final NodeType node2);
 }
