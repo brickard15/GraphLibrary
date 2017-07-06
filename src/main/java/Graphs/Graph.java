1f9fc2ca658f3ed6444b064f6a776d6c10f4fe2b
brickard15 <brian.rick@gmail.com>
2017-07-05 23:54:52 -0500

    Changed the addEdge() definition to accept two nodeIds and edgeData.

--- src/main/java/Graphs/Graph.java
+++ src/main/java/Graphs/Graph.java
@@ -5,5 +5,5 @@ public interface Graph <NodeType, NodeIdType, EdgeType>{
     public boolean hasNode(final NodeIdType nodeId);
     public boolean hasEdge(final NodeIdType nodeIdType1, final NodeIdType nodeIdType2);
     public void addNode(final NodeType nodeData, NodeIdType nodeId);
-    public void addEdge(final NodeType node1, final NodeType node2);
+    public void addEdge(final NodeIdType nodeId1, final NodeIdType NodeId2, final EdgeType edgeData);
 }
