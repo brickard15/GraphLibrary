37db61281f8624363e686a36df669187f2b98484
brickard15 <brian.rick@gmail.com>
2017-07-05 23:50:12 -0500

    Changed the hasEdge method definition.

--- src/main/java/Graphs/Graph.java
+++ src/main/java/Graphs/Graph.java
@@ -3,7 +3,7 @@ package Graphs;
 public interface Graph <NodeType, NodeIdType, EdgeType>{
 
     public boolean hasNode(final NodeIdType nodeId);
-    public boolean hasEdge(final NodeType node1, final NodeType node2);
+    public boolean hasEdge(final NodeIdType nodeIdType1, final NodeIdType nodeIdType2);
     public void addNode(final NodeType node);
     public void addEdge(final NodeType node1, final NodeType node2);
 }
