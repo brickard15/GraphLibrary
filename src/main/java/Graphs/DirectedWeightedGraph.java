f85db380f429a7745d3138077aaba13c869470ee
brickard15 <brian.rick@gmail.com>
2017-07-05 23:57:49 -0500

    Changed the method definitions to match that of the new Graph interface.

--- src/main/java/Graphs/DirectedWeightedGraph.java
+++ src/main/java/Graphs/DirectedWeightedGraph.java
@@ -6,38 +6,31 @@ import NodeIdGenerators.NodeIdGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DirectedWeightedGraph <NodeType, NodeIdType, EdgeType> implements Graph <NodeType, EdgeType> {
+public class DirectedWeightedGraph <NodeType, NodeIdType, EdgeType> implements Graph <NodeType, NodeIdType, EdgeType> {
     private final List<Node<NodeType, NodeIdType, EdgeType>> nodes;
 
     public DirectedWeightedGraph(){
         nodes = new ArrayList<>();
     }
 
-    @Override
-    public boolean hasNode(final Node node) {
-        final boolean[] isPresent = {false};
-
-        nodes.forEach((possibleNode) -> {
-            if (node.equals(possibleNode)){
-                isPresent[0] = true;
-            }
-        });
-
-        return isPresent[0];
-    }
 
     @Override
-    public boolean hasEdge(final NodeType node1, final NodeType node2) {
+    public boolean hasNode(Object nodeId) {
         return false;
     }
 
     @Override
-    public void addNode(final NodeType node) {
+    public boolean hasEdge(Object nodeIdType1, Object nodeIdType2) {
+        return false;
+    }
+
+    @Override
+    public void addNode(Object nodeData, Object nodeId) {
 
     }
 
     @Override
-    public void addEdge(final NodeType node1, final NodeType node2) {
+    public void addEdge(Object nodeId1, Object NodeId2, Object edgeData) {
 
     }
 }
