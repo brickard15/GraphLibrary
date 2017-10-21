4cf939932f2a403d0e417ee479bbee789c5263a2
brickard15 <brian.rick@gmail.com>
2017-10-21 11:54:41 -0500

    Rearranged the NodeIdType to the be the first in parameter in method definitions and Type declarations in the Graph interface.
    This provides some consistency in the interfaces and classes.

--- src/main/java/Graphs/Graph.java
+++ src/main/java/Graphs/Graph.java
@@ -1,9 +1,9 @@
 package Graphs;
 
-public interface Graph <NodeType, NodeIdType, EdgeType>{
+public interface Graph <NodeIdType, NodeType, EdgeType>{
 
     public boolean hasNode(final NodeIdType nodeId);
     public boolean hasEdge(final NodeIdType nodeIdType1, final NodeIdType nodeIdType2);
-    public void addNode(final NodeType nodeData, NodeIdType nodeId);
+    public void addNode(NodeIdType nodeId, final NodeType nodeData);
     public void addEdge(final NodeIdType nodeId1, final NodeIdType NodeId2, final EdgeType edgeData);
 }
