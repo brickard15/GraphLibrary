07273a1761f0401ad965d5643c657eeb93513fd8
brickard15 <brian.rick@gmail.com>
2017-11-06 22:39:00 -0600

    Implemented the UndirectedUnweightedGraph methods addNode, hasNode, addEdge, hasEdge, getEdgeData, and getOutDegree. Created tests in the UndirectedUnweightedGraphTest.

--- src/main/java/Graphs/UndirectedUnweightedGraph.java
+++ src/main/java/Graphs/UndirectedUnweightedGraph.java
@@ -1,4 +1,54 @@
 package Graphs;
 
-public class UndirectedUnweightedGraph {
+import java.util.Optional;
+
+public class UndirectedUnweightedGraph<NodeId, NodeData, EdgeData> implements Graph<NodeId, NodeData, EdgeData> {
+    private Graph<NodeId, NodeData, EdgeData> graph;
+
+    public UndirectedUnweightedGraph(){
+        graph = new DirectedWeightedGraph<>();
+    }
+
+    @Override
+    public void addNode(NodeId nodeId, NodeData nodeData) {
+        graph.addNode(nodeId, nodeData);
+    }
+
+    @Override
+    public boolean hasNode(NodeId nodeId) {
+        return graph.hasNode(nodeId);
+    }
+
+    @Override
+    public Optional<NodeData> getNodeData(NodeId nodeId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+
+    @Override
+    public void addEdge(NodeId nodeId1, NodeId nodeId2, EdgeData edgeData) {
+        if (graph.hasNode(nodeId1) && graph.hasNode(nodeId2)){
+            graph.addEdge(nodeId1, nodeId2, edgeData);
+            graph.addEdge(nodeId2, nodeId1, edgeData);
+        }
+    }
+
+    @Override
+    public boolean hasEdge(NodeId nodeId1, NodeId nodeId2) {
+        return graph.hasEdge(nodeId1, nodeId2) && graph.hasEdge(nodeId2, nodeId1);
+    }
+
+    @Override
+    public Optional<EdgeData> getEdgeData(NodeId node1Id, NodeId node2Id) {
+        return graph.getEdgeData(node1Id, node2Id);
+    }
+
+    @Override
+    public int getOutDegree(NodeId nodeId) {
+        return graph.getOutDegree(nodeId);
+    }
 }
--- src/test/java/Graphs/UndirectedUnweightedGraphTest.java
+++ src/test/java/Graphs/UndirectedUnweightedGraphTest.java
@@ -1,5 +1,85 @@
 package Graphs;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UndirectedUnweightedGraphTest {
 
+    private Graph<String, String, String> testGraph;
+    private Graph<String, String, String> testGraphWith2Nodes;
+    private static final String CITY_1_ID = "City1";
+    private static final String CITY_2_ID = "City2";
+
+
+    @Before
+    public void SetupNewGraph(){
+        testGraph = new UndirectedUnweightedGraph();
+        testGraphWith2Nodes = new UndirectedUnweightedGraph();
+        testGraphWith2Nodes.addNode(CITY_1_ID, "Murray, KY");
+        testGraphWith2Nodes.addNode(CITY_2_ID, "Elkton, KY");
+    }
+
+    @Test
+    public void hasNodeWithNoNodes(){
+        Assert.assertFalse(testGraph.hasNode("City1"));
+    }
+
+    @Test
+    public void AddSingleNode(){
+        testGraph.addNode(CITY_1_ID, "Murray, KY");
+        Assert.assertTrue(testGraph.hasNode("City1"));
+    }
+
+    @Test
+    public void AddTwoNodes(){
+        Assert.assertTrue(testGraphWith2Nodes.hasNode(CITY_1_ID));
+        Assert.assertTrue(testGraphWith2Nodes.hasNode(CITY_2_ID));
+    }
+
+    @Test
+    public void hasEdgeWithNoEdges(){
+        Assert.assertFalse(testGraphWith2Nodes.hasEdge(CITY_1_ID, CITY_2_ID));
+        Assert.assertFalse(testGraphWith2Nodes.hasEdge(CITY_2_ID, CITY_1_ID));
+    }
+
+    @Test
+    public void addOneEdge(){
+        testGraphWith2Nodes.addEdge(CITY_1_ID, CITY_2_ID, "Interstate 40");
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_1_ID, CITY_2_ID));
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_2_ID, CITY_1_ID));
+    }
+
+    @Test
+    public void addEdgeWithJustOneNode(){
+        testGraph.addNode(CITY_1_ID, "Murray, KY");
+        testGraph.addEdge(CITY_1_ID, CITY_2_ID, "I40");
+        Assert.assertFalse(testGraph.hasEdge(CITY_1_ID, CITY_2_ID));
+        Assert.assertFalse(testGraph.hasEdge(CITY_2_ID, CITY_1_ID));
+    }
+
+    @Test
+    public void addEdgeBetweenSameNodeTwice(){
+        testGraphWith2Nodes.addEdge(CITY_1_ID, CITY_2_ID, "I40");
+        testGraphWith2Nodes.addEdge(CITY_1_ID, CITY_2_ID, "I24");
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_1_ID, CITY_2_ID));
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_2_ID, CITY_1_ID));
+        Assert.assertEquals("I40", testGraphWith2Nodes.getEdgeData(CITY_1_ID, CITY_2_ID).get());
+        Assert.assertEquals("I40", testGraphWith2Nodes.getEdgeData(CITY_2_ID, CITY_1_ID).get());
+        Assert.assertEquals(1, testGraphWith2Nodes.getOutDegree(CITY_1_ID));
+        Assert.assertEquals(1, testGraphWith2Nodes.getOutDegree(CITY_2_ID));
+    }
+
+    @Test
+    public void addEdgeBetweenSameNodeInDifferentDirections(){
+        testGraphWith2Nodes.addEdge(CITY_2_ID, CITY_1_ID, "I24");
+        testGraphWith2Nodes.addEdge(CITY_1_ID, CITY_2_ID, "I40");
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_1_ID, CITY_2_ID));
+        Assert.assertTrue(testGraphWith2Nodes.hasEdge(CITY_2_ID, CITY_1_ID));
+        Assert.assertEquals("I24", testGraphWith2Nodes.getEdgeData(CITY_1_ID, CITY_2_ID).get());
+        Assert.assertEquals("I24", testGraphWith2Nodes.getEdgeData(CITY_2_ID, CITY_1_ID).get());
+        Assert.assertEquals(1, testGraphWith2Nodes.getOutDegree(CITY_1_ID));
+        Assert.assertEquals(1, testGraphWith2Nodes.getOutDegree(CITY_2_ID));
+
+    }
 }
