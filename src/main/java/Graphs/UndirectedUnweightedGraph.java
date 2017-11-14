f72a7a2ba98994e8d56de06d0173126bb850f053
brickard15 <brian.rick@gmail.com>
2017-11-13 23:40:04 -0600

    Changed the UndirectedUnweightedGraph to actually be unweighted. Changed the test to reflect the new interface.

--- src/main/java/Graphs/UndirectedUnweightedGraph.java
+++ src/main/java/Graphs/UndirectedUnweightedGraph.java
@@ -2,52 +2,41 @@ package Graphs;
 
 import java.util.Optional;
 
-public class UndirectedUnweightedGraph<NodeId, NodeData, EdgeData> implements Graph<NodeId, NodeData, EdgeData> {
-    private Graph<NodeId, NodeData, EdgeData> graph;
+public class UndirectedUnweightedGraph<NodeId, NodeData>{
+    private Graph<NodeId, NodeData, Integer> graph;
+    private static final int defaultEdgeData = 1;
 
     public UndirectedUnweightedGraph(){
         graph = new DirectedWeightedGraph<>();
     }
 
-    @Override
     public void addNode(NodeId nodeId, NodeData nodeData) {
         graph.addNode(nodeId, nodeData);
     }
 
-    @Override
     public boolean hasNode(NodeId nodeId) {
         return graph.hasNode(nodeId);
     }
 
-    @Override
     public Optional<NodeData> getNodeData(NodeId nodeId) {
         return graph.getNodeData(nodeId);
     }
 
-    @Override
     public int getNodeCount() {
         return graph.getNodeCount();
     }
 
-    @Override
-    public void addEdge(NodeId nodeId1, NodeId nodeId2, EdgeData edgeData) {
+    public void addEdge(NodeId nodeId1, NodeId nodeId2) {
         if (graph.hasNode(nodeId1) && graph.hasNode(nodeId2)){
-            graph.addEdge(nodeId1, nodeId2, edgeData);
-            graph.addEdge(nodeId2, nodeId1, edgeData);
+            graph.addEdge(nodeId1, nodeId2, defaultEdgeData);
+            graph.addEdge(nodeId2, nodeId1, defaultEdgeData);
         }
     }
 
-    @Override
     public boolean hasEdge(NodeId nodeId1, NodeId nodeId2) {
         return graph.hasEdge(nodeId1, nodeId2) && graph.hasEdge(nodeId2, nodeId1);
     }
 
-    @Override
-    public Optional<EdgeData> getEdgeData(NodeId node1Id, NodeId node2Id) {
-        return graph.getEdgeData(node1Id, node2Id);
-    }
-
-    @Override
     public int getOutDegree(NodeId nodeId) {
         return graph.getOutDegree(nodeId);
     }
