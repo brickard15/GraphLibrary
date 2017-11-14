c0411e6cfb2841920a289826b7e7ebdc9ecc071d
brickard15 <brian.rick@gmail.com>
2017-11-13 18:27:17 -0600

    Implemented an addOneNode test, and implemented to the addNode, hasNode, and getNodeData from the Graph interface.

--- src/main/java/Graphs/DirectedUnweightedGraph.java
+++ src/main/java/Graphs/DirectedUnweightedGraph.java
@@ -1,4 +1,53 @@
 package Graphs;
 
-public class DirectedUnweightedGraph {
+import java.util.Optional;
+
+public class DirectedUnweightedGraph<NodeId, NodeData, EdgeData> implements Graph<NodeId, NodeData, EdgeData> {
+
+    private final Graph<NodeId, NodeData, EdgeData> graph;
+
+    public DirectedUnweightedGraph(){
+        graph = new DirectedWeightedGraph<>();
+    }
+
+
+    @Override
+    public void addNode(NodeId nodeId, NodeData nodeData) {
+        graph.addNode(nodeId, nodeData);
+    }
+
+    @Override
+    public boolean hasNode(NodeId nodeId) {
+        return graph.hasNode(nodeId);
+    }
+
+    @Override
+    public Optional<NodeData> getNodeData(NodeId nodeId) {
+        return graph.getNodeData(nodeId);
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+
+    @Override
+    public void addEdge(NodeId nodeId1, NodeId nodeId2, EdgeData edgeData) {
+
+    }
+
+    @Override
+    public boolean hasEdge(NodeId nodeId1, NodeId nodeId2) {
+        return false;
+    }
+
+    @Override
+    public Optional<EdgeData> getEdgeData(NodeId node1Id, NodeId node2Id) {
+        return null;
+    }
+
+    @Override
+    public int getOutDegree(NodeId nodeId) {
+        return 0;
+    }
 }
--- src/test/java/Graphs/DirectedUnweightedGraphTests.java
+++ src/test/java/Graphs/DirectedUnweightedGraphTests.java
@@ -1,4 +1,19 @@
 package Graphs;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.*;
+
 public class DirectedUnweightedGraphTests {
+
+    @Test
+    public void addOneNode(){
+        Graph<Integer, Point, Double> testGraph = new DirectedUnweightedGraph();
+        Point point1 =  new Point(10,10);
+        testGraph.addNode(1, point1);
+        Assert.assertTrue(testGraph.hasNode(1));
+        Assert.assertTrue(testGraph.getNodeData(1).isPresent());
+        Assert.assertEquals(point1, testGraph.getNodeData(1).get());
+    }
 }
