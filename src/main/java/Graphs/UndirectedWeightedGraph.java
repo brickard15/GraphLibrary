91bc476b8545a1f119f0e5807ed6eb826147e8e5
brickard15 <brian.rick@gmail.com>
2017-11-14 21:15:00 -0600

    Implemented an addOneEdge test that required implementing the addNode, addEdge, and hasEdge methods in the UndirectedWeightedGraph.

--- src/main/java/Graphs/UndirectedWeightedGraph.java
+++ src/main/java/Graphs/UndirectedWeightedGraph.java
@@ -1,4 +1,53 @@
 package Graphs;
 
-public class UndirectedWeightedGraph {
+import java.util.Optional;
+
+public class UndirectedWeightedGraph <NodeId, NodeData, EdgeData> implements Graph<NodeId, NodeData, EdgeData> {
+
+    private Graph<NodeId, NodeData, EdgeData> graph;
+
+    public UndirectedWeightedGraph(){
+        graph = new DirectedWeightedGraph<>();
+    }
+
+    @Override
+    public void addNode(NodeId nodeId, NodeData nodeData) {
+        graph.addNode(nodeId, nodeData);
+    }
+
+    @Override
+    public boolean hasNode(NodeId nodeId) {
+        return false;
+    }
+
+    @Override
+    public Optional<NodeData> getNodeData(NodeId nodeId) {
+        return null;
+    }
+
+    @Override
+    public int getNodeCount() {
+        return 0;
+    }
+
+    @Override
+    public void addEdge(NodeId nodeId1, NodeId nodeId2, EdgeData edgeData) {
+        graph.addEdge(nodeId1, nodeId2, edgeData);
+        graph.addEdge(nodeId2, nodeId1, edgeData);
+    }
+
+    @Override
+    public boolean hasEdge(NodeId nodeId1, NodeId nodeId2) {
+        return graph.hasEdge(nodeId1, nodeId2);
+    }
+
+    @Override
+    public Optional<EdgeData> getEdgeData(NodeId node1Id, NodeId node2Id) {
+        return null;
+    }
+
+    @Override
+    public int getOutDegree(NodeId nodeId) {
+        return 0;
+    }
 }
--- src/test/java/Graphs/UndirectedWeightedGraphTests.java
+++ src/test/java/Graphs/UndirectedWeightedGraphTests.java
@@ -1,4 +1,33 @@
 package Graphs;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UndirectedWeightedGraphTests {
+
+    private UndirectedWeightedGraph<Integer, Integer, Integer> graph;
+    private UndirectedWeightedGraph<Integer, Integer, Integer> graphWith3Nodes;
+
+    @Before
+    public void SetupGraph(){
+        graph = new UndirectedWeightedGraph<>();
+    }
+
+    @Before
+    public void SetupGraphWith3Nodes(){
+        graphWith3Nodes = new UndirectedWeightedGraph<>();
+        graphWith3Nodes.addNode(1, 10);
+        graphWith3Nodes.addNode(2, 20);
+        graphWith3Nodes.addNode(3, 30);
+    }
+
+    @Test
+    public void addOneEdge(){
+        graphWith3Nodes.addEdge(1, 2, 100);
+        Assert.assertTrue(graphWith3Nodes.hasEdge(1, 2));
+        Assert.assertTrue(graphWith3Nodes.hasEdge(2, 1));
+    }
+
+
 }
